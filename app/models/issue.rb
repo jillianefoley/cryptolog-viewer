531a099efc3de92d5fc31b1dfcec131e2025e98f
Jillian <jillianefoley@gmail.com>
2016-02-17 01:28:36 -0600

    Made article index less bad

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,12 @@
 class Issue < ActiveRecord::Base
 		belongs_to :person, :foreign_key => 'editor_id'
+		has_many :articles
+
+		def name
+			"Vol. #{volume} Num. #{volume_number}, #{month} #{year}"
+		end
+
+		def date
+			"#{month} #{year}"
+		end
 end
